09bd7e452ad3d6d4640578c16110b7897e0a088d
Marin Procureur <marin.procureur@gmail.com>
2019-04-25 11:57:37 +0200

    First commit

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <ul class="list-reset flex lg:fixed">
+      <li class="mr-6">
+        <nuxt-link
+          to="/"
+          class="no-underline block p-2 text-blue hover:text-blue-darker"
+          >Home
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class NewComponent extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
